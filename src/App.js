5b83cdf4e9ca5cde7acecab943a0dffa1970005a
SauravMishra100 <sm2243@scarletmail.rutgers.edu>
2021-06-22 20:36:13 -0400

    penis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [busData, setBusData] = useState(null)
+  const routes = {
+    4014680: "Summer 1",
+    4014682: "Summer 2",
+  }
+
+  const busApi = async () => {
+    await fetch("https://transloc-api-1-2.p.rapidapi.com/arrival-estimates.json?agencies=1323&stops=4231636",{
+      headers : {
+        "x-rapidapi-key": "174e52e98bmshe16deea62c1c223p1ee364jsn75d1c04fbc8b",
+        "x-rapidapi-host": "transloc-api-1-2.p.rapidapi.com",
+        "useQueryString": true
+      }
+    })
+      .then(res => res.json())
+      .then(json => setBusData(json.data.arrivals));
+  }
+
+
+  const BusTimes = () => {
+    return busData.map((b, i) => <BusTime key={i} busName={routes[b.route_id]} busTime={b.arrival_at}/>)
+  }
+
+  const BusTime = ({ busName, busTime }) => {
+    return <div>{busName} : {busTime}</div>
+  }
+  
+  if(!busData){
+    busApi()
+    console.log(busData)
+  } else {
+    setInterval(function() {
+      busApi()
+      console.log(busData)
+  }, 5 * 1000)
+  }
+
+  return busData ? (<div><BusTimes /></div>) : <h1>:(</h1>
 }
 
 export default App;
